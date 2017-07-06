aac7a872eed656aec451574487f1610cc072621f
nick fan <nickfan81@gmail.com>
2017-07-06 17:27:11 +0800

    tweak preload

--- optdemoy/resources/views/main.blade.php
+++ optdemoy/resources/views/main.blade.php
@@ -4,7 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv='x-dns-prefetch-control' content='on'>
+    <link rel="dns-prefetch" href="http://optdemocdn.com">
+    <link rel="preconnect" href="http://optdemocdn.com">
     <link rel="subresource" href="http://optdemocdn.com/assets/plugins/jquery/1.12.4/jquery.min.js">
+    <link rel="subresource" href="http://optdemocdn.com/assets/img/grey.gif">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="http://optdemocdn.com/assets/img/favicon.ico">
